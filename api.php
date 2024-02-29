90b2601bc9a3de491bd6210cee8df524c411f961
Shah jalal <rjshahjalal132@gmail.com>
2024-02-29 11:57:38 +0600

    moved helper functions to custom function file

--- api.php
+++ api.php
@@ -78,49 +78,6 @@ function test_api_admin_callback() {
     }
 }
 
-/**
- * Function to save response data to a file.
- *
- * @param string $data The response data to be saved.
- */
-function put_response_data( $data ) {
-    // Ensure directory exists to store response data
-    $directory = API_PLUGIN_PATH . '/data/';
-    if ( !file_exists( $directory ) ) {
-        mkdir( $directory, 0777, true );
-    }
 
-    // Construct file path for response data
-    $fileName = $directory . 'response.json';
-
-    // Write response data to file
-    if ( file_put_contents( $fileName, $data ) !== false ) {
-        return "Data written to file successfully.";
-    } else {
-        return "Failed to write data to file.";
-    }
-}
-
-/**
- * Write an error message to a file.
- *
- * @param string $error_message The error message to be written to the file.
- * @return string Return a success or failure message.
- */
-function put_error_message( $error_message ) {
-    // Ensure directory exists to store error logs
-    $directory = API_PLUGIN_PATH . '/errors/';
-    if ( !file_exists( $directory ) ) {
-        mkdir( $directory, 0777, true );
-    }
-
-    // Construct file path for error log
-    $fileName = $directory . 'error-log.txt';
-
-    // Write error message to file
-    if ( file_put_contents( $fileName, $error_message ) !== false ) {
-        return "Error message written to file successfully.";
-    } else {
-        return "Failed to write error message to file.";
-    }
-}
+// include required files
+require_once API_PLUGIN_PATH . '/inc/api_custom_functions.php';
